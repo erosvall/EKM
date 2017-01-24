84ed05836b3f5f609c2bb35056300921a489430a
Erik Rosvall <erik.rosvall@gmail.com>
2017-01-24 11:57:21 +0100

    centerlight existerar visst

--- v0.1/gaussian_face_detection.m
+++ v0.1/gaussian_face_detection.m
@@ -1,6 +1,6 @@
 %% Add Images to working directory
 % Make sure path is correct for your computer. But not you Viktor
-path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/mattraining/';
+path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/training/';
 addpath(genpath(path));
 
 
@@ -8,18 +8,30 @@ addpath(genpath(path));
 clc
 %mshow('s1p1.gif');
 IV = []; 
-A = rand(100,77760);
+A = rand(8,77760);
+fileEnding = {'centerlight','glasses','happy','leftlight','noglasses','normal','rightlight','sad','sleepy'};
 for i = 1:1
-    for k = 1:10
-        imgFileName = strcat('s',num2str(i),'p',num2str(k),'.gif');
+    for k = 1:9
+        imgFileName = strcat('subject0',num2str(i),'.',fileEnding{1,k}); 
         img = imread(imgFileName);
         j = im2double(img);
         IV = [IV A*j(:)];
     end
-    
-    options = statset('Display','final');
-    GMM = fitgmdist(IV,1,'Options',options,'Regularize',0.00001);
+    GMM = fitgmdist(IV',1,'Regularize',0.00001);
 
 end
 
 %% Generate test system
+path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/test/';
+addpath(genpath(path));
+
+fileEndingTest = {'wink','surprised'};
+fileDataTest = string(fileEndingTest);
+
+imgFileName = strcat('subject01.wink');
+img = imread(imgFileName);
+j = im2double(img);
+X = A*j(:);
+
+y = mvnpdf(X,GMM.mu, GMM.Sigma);
+posterior(GMM,X')
