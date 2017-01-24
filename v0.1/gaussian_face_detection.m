3da7aadd5d6a983ab6fb434924c6fab4538963cf
Erik Rosvall <erik.rosvall@gmail.com>
2017-01-24 09:17:45 +0100

    Första commiten för första arbetet

---
+++ v0.1/gaussian_face_detection.m
@@ -0,0 +1,7 @@
+%% Add Images to working directory
+% Make sure path is correct for your computer. But not you Viktor
+addpath(genpath('/Users/erikrosvall/Dropbox/Kex/yalefaces'));
+
+%% Generate training system
+imshow('subject01.gif');
+%% Generate test system
