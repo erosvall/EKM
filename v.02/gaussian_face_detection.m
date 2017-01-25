1255840dfd7fc79318000f64fc8995ed628bdeda
Viktor Karlsson <Viktor2k>
2017-01-25 17:23:01 +0100

    klar med modul 1, fungerande klassificiering

--- v.02/gaussian_face_detection.m
+++ v.02/gaussian_face_detection.m
@@ -1,7 +1,7 @@
 %% Add Images to working directory
 % Make sure path is correct for your computer. But not you Viktor
-path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/training/';
-%path = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/training/';
+%path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/training/';
+path = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/training/';
 addpath(genpath(path));
 
 
@@ -9,25 +9,28 @@ addpath(genpath(path));
 
 %% Generate training system
 clc
-
-%mshow('s1p1.gif');
 imageVector = []; 
-%imageVector = vpa(imageVector);
+
 MU = [];
 SIGMA = [];
-numbOfClasses = 9;
-downSampling = 777;
-c = 10e-3;
+numbOfClasses = 15;
+SizeOfVector = 500; % to high dimension leads to ill-contitiond sigma
+
+c = 10e-3; %solving ill-condition of SIGMA
 
 A = rand(512,77760);
 fileEnding = {'centerlight','glasses','happy','leftlight','noglasses','normal','rightlight','sad','sleepy'};
 for i = 1:numbOfClasses
     mu = [];
     for k = 1:9
-        imgFileName = strcat('subject0',num2str(i),'.',fileEnding{1,k}); 
+        if(i<10)
+            imgFileName = strcat('subject0',num2str(i),'.',fileEnding{1,k}); 
+        else
+            imgFileName = strcat('subject',num2str(i),'.',fileEnding{1,k}); 
+        end
         img = imread(imgFileName);
         j = im2double(img);
-        imageVector = [imageVector resample(j(:),1,downSampling)];
+        imageVector = [imageVector resample(j(:),SizeOfVector,77760)];
     end
     
     for l = 1:length(imageVector(:,1))
@@ -42,12 +45,18 @@ for i = 1:numbOfClasses
     
     MU = [MU mu];
     
-    disp('Done with class')
+    disp(strcat('Done with class: ',num2str(i)))
 end
-disp('Done with modelling')
-%% Generate test system
-path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/test/';
-%path = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/test/';
+
+
+disp(' ')
+disp('Done with modelling!')
+disp(' ')
+
+% Generate test system
+%clc
+%path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/test/';
+path = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/test/';
 prob = [];
 
 addpath(genpath(path));
@@ -55,17 +64,38 @@ addpath(genpath(path));
 fileEndingTest = {'wink','surprised'};
 fileDataTest = string(fileEndingTest);
 
-imgFileName = strcat('subject02.wink');
-img = imread(imgFileName);
+for i = 1:2 %looping over fileEndings
+    correctCounter = 0;
+    
+    disp(fileEndingTest{1,i})
+    disp('------------')
+    for j = 1:numbOfClasses % for every person 
+        disp(strcat('person: ',num2str(j)))
+        testVector = [];
+        prob = [];
+        if(j<10)
+            imgFileName = strcat('subject0',num2str(j),'.',fileEndingTest{1,i}); 
+        else
+            imgFileName = strcat('subject',num2str(j),'.',fileEndingTest{1,i}); 
+        end
+        %imgFileName = strcat('subject0',num2str(j),'.',fileEndingTest{1,i}); 
+        img = imread(imgFileName);
+        testImage = im2double(img);
+        testVector = resample(testImage(:),SizeOfVector,77760);
 
-testImage = im2double(img);
-testVector = resample(testImage(:),1,downSampling);
-
-prob = [];
-
-for i = 1:numbOfClasses
-    prob = [prob mvnpdf(testVector, MU(:,i), SIGMA(:,:,i))];
-    cond(SIGMA(:,:,i))
+        for k = 1:numbOfClasses %looping through the model
+            prob = [prob mvnpdf(testVector, MU(:,k), SIGMA(:,:,k))];
+            
+        end
+        
+        disp(strcat('classified as person: ', num2str(find(prob == max(prob(:))))))
+        
+        if(j == find(prob == max(prob(:))))
+            correctCounter = correctCounter + 1;
+        end
+    end
+    disp(' ' )
+    disp(strcat('Accuracy of prediction:', num2str(100*correctCounter/numbOfClasses),'%'))
+    disp('------------')
 end
-
-disp(prob);
+disp('send nudes')
