a8d3420f7c8499224cf0fd2939fda463c716375d
Viktor Karlsson <Viktor2k>
2017-02-03 12:33:59 +0100

    VIktor lämnar över gauss till erik och börjar med omp

--- v.02/gaussian_face_detection.m
+++ v.02/gaussian_face_detection.m
@@ -53,13 +53,13 @@ disp(' ')
 disp('Done with modelling!')
 disp(' ')
 
-% Generate test system
+%% Generate test system
 %clc
 %path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/test/';
-path = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/test/';
+path1 = '/Users/Viktor/Dropbox/KTH/År 3/Period 3/Kex/yalefaces/test/';
 prob = [];
 
-addpath(genpath(path));
+addpath(genpath(path1));
 
 fileEndingTest = {'wink','surprised'};
 fileDataTest = string(fileEndingTest);
@@ -98,4 +98,50 @@ for i = 1:2 %looping over fileEndings
     disp(strcat('Accuracy of classification:', num2str(100*correctCounter/numbOfClasses),'%'))
     disp('------------')
 end
-disp('send nudes')
+%% Test på bilder i träningsdata
+%path = '/Users/erikrosvall/Dropbox/Kex/yalefaces/test/';
+prob = [];
+
+addpath(genpath(path));
+
+fileEndingTest = {'centerlight','glasses'};
+fileDataTest = string(fileEndingTest);
+
+for i = 1:2 %looping over fileEndings
+    correctCounter = 0;
+    
+    disp(fileEndingTest{1,i})
+    disp('------------')
+    for j = 1:numbOfClasses % for every person 
+        disp(strcat('person: ',num2str(j)))
+        testVector = [];
+        prob = [];
+        if(j<10)
+            imgFileName = strcat('subject0',num2str(j),'.',fileEndingTest{1,i}); 
+        else
+            imgFileName = strcat('subject',num2str(j),'.',fileEndingTest{1,i}); 
+        end
+        %imgFileName = strcat('subject0',num2str(j),'.',fileEndingTest{1,i}); 
+        img = imread(imgFileName);
+        testImage = im2double(img);
+        testVector = resample(testImage(:),SizeOfVector,77760);
+
+        for k = 1:numbOfClasses %looping through the model
+            prob = [prob mvnpdf(testVector, MU(:,k), SIGMA(:,:,k))];
+            
+        end
+        
+        disp(strcat('classified as person: ', num2str(find(prob == max(prob(:))))))
+        
+        if(j == find(prob == max(prob(:))))
+            correctCounter = correctCounter + 1;
+        end
+    end
+    disp(' ' )
+    disp(strcat('Accuracy of classification:', num2str(100*correctCounter/numbOfClasses),'%'))
+    disp('------------')
+end
+
+
+
+
