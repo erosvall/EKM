9faf794c70047103b422f6273cc66c9050297408
Viktor Karlsson <Viktor2k>
2017-02-07 08:31:44 +0100

    Ä
    git commit -m Ä

---
+++ Dictionary Learning/ViktorTries.m
@@ -0,0 +1,34 @@
+%% Viktors försök på en OMP + K-SVD dictionary learning algoritm
+n = 10; % dim på data
+L = 2; % antal data
+m = 20; % storlek på dictionary
+s = 4; % sparcity
+
+X = rand(n,L); % data 
+D = normc(rand(n,m)); % dictionary med normerade atomer
+A = []; % sparce representation av vektor
+
+
+
+% Steg 1: Sparce coding genom OMP --------------------------
+
+I = []; %Sparar utvalda index
+
+for i = 1:L % för varje vektor i data-set
+    r = X(:,i); %residualen 
+    D0 = []; %Utvalda atomer sparas för denna datavektor
+    tempI = []; %Utvalda atomers idex sparas för denna datavetktor
+    for j = 1:s %
+        [maxval, maxarg] = max(abs(r'*D)); %hittar bästa projektionen
+        tempI = [tempI maxarg]; %använda atomer från D
+        D0 = [D0 D(:,maxarg)]; %sparar den bästa atomen från denna iteration  
+        a = (D0'*D0)\(D0'*X(:,i)); %projicerar x på det som späns upp av alla hitils utvalda atomer 
+        P = sum(D0*a,2);
+        r = X(:,i) - P;
+        norm(r)
+    end
+    A = [A a];
+    I = [I; tempI']
+    
+end
+
