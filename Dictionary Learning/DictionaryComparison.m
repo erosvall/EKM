ceb79ab3e7bfb701fc0e54bd156c6d6b5cddebbc
Viktor Karlsson <Viktor2k>
2017-02-08 16:51:41 +0100

    ändrade diffperelem till diffpercol

--- Dictionary Learning/DictionaryComparison.m
+++ Dictionary Learning/DictionaryComparison.m
@@ -1,4 +1,4 @@
-function [ totalDiff, diffPerElement ] = DictionaryComparison( D, D0 )
+function [ totalDiff, diffPerCol ] = DictionaryComparison( D, D0 )
 % Comparing similary between original dicionary D0 and generated dictionary
 % D. Works only when D and D0 have normalized columns
 % 
@@ -10,6 +10,6 @@ for i = 1:size(D,2)
     totalDiff = totalDiff + (1-maxval);
 end
 
-diffPerElement= totalDiff/size(D,2);
+diffPerCol= totalDiff/size(D,2);
 end
 
