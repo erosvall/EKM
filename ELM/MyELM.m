e120e1fd8c67b74d374df759a01d4ed2b30970bf
Viktor Karlsson <Viktor2k>
2017-03-07 17:38:35 +0100

    egen elm färdig grund

---
+++ ELM/MyELM.m
@@ -0,0 +1,43 @@
+% Pleb way of ELM
+%% Loading files
+imagesTrain = loadMNISTImages('train-images.idx3-ubyte');
+labelsTrain = loadMNISTLabels('train-labels.idx1-ubyte');
+
+imagesTest = loadMNISTImages('t10k-images.idx3-ubyte');
+labelsTest = loadMNISTLabels('t10k-labels.idx1-ubyte');
+
+%% Training!
+N = 60000; %number of datapoints
+h = 2000; % number of hidden layers
+
+
+
+X = imagesTrain(1:end,1:N);% Smaller feature matrix 
+sublables = labelsTrain(1:N,1);  % correspoinding lables
+
+B = ones(h,size(X,2)); % Initialize bias term
+Wr = dictmake( h , size(X,1)); % random weights
+sigma = 1./(1+exp(Wr*X + B)); % sigmoid function
+
+Y = zeros(10,size(X,2));% Initialize class matrix
+%fill class matrix
+for i = 1:size(sublables,1)
+    Y(sublables(i,1)+1,i) = 1;
+end
+
+W1 = Y*pinv(sigma); % Calculated weiths in output layer
+
+
+%% Testing
+Xtest = imagesTest;
+Ytest = labelsTest';
+Btest = ones(h,size(Xtest,2));
+
+sigmatest = 1./(1+exp(Wr*Xtest + Btest));
+
+Yres = W1 * sigmatest;
+[maxval maxind] = max(Yres,[],1);
+maxind = maxind-1;
+
+accuracy = nnz(Ytest == maxind)/size(Ytest,2)
+
