832f847330db316bde06fc8f65bf3e85f71fba77
Viktor Karlsson <Viktor2k>
2017-03-21 18:15:03 +0100

    sparar nu yale och mnist data som kan läsas direkt

--- Functions/main.m
+++ Functions/main.m
@@ -1,16 +1,28 @@
-imagesTrain = loadMNISTImages('train-images.idx3-ubyte');
-labelsTrain = loadMNISTLabels('train-labels.idx1-ubyte');
 
-imagesTest = loadMNISTImages('t10k-images.idx3-ubyte');
-labelsTest = loadMNISTLabels('t10k-labels.idx1-ubyte');
-%% ELM
-datasize = 6000;
+%% ELM on MNIST
+clear all
+load MNISTData.mat
+
+datasize = 6000;  %max 60000
+hiddenLayers = 1000;
 
 X = imagesTrain(:,1:datasize);
 L = labelsTrain(1:datasize,1)';
-[Wi, Wo] = ELMtrain(X,L,1000);
+[Wi, Wo] = ELMtrain(X,L,hiddenLayers);
 
 testL = ELMclassifier(imagesTest,Wi,Wo);
 accuracy = nnz(testL == labelsTest')/size(testL,2)
 
-%% 
+%% ELM on YaleFaces
+clear all
+load yalefaceData.mat
+
+datasize = 135;
+hiddenLayers = 1000;
+
+X = yaleFeatures(:,1:datasize);
+L = yaleLabels(1,1:datasize);
+[Wi, Wo] = ELMtrain(X,L,hiddenLayers);
+
+testL = ELMclassifier(yaleFeatures(:,datasize+1:end),Wi,Wo);
+accuracy = nnz(testL == yaleLabels(1,datasize+1:end))/size(testL,2)
