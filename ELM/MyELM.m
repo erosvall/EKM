a4fd04d56524111f4224ea06f38d494b9ce94f7b
Viktor Karlsson <Viktor2k>
2017-03-21 15:03:00 +0100

    RLU implementerad, några osäkerheter i hur bias-termen ska hanteras

--- ELM/MyELM.m
+++ ELM/MyELM.m
@@ -6,34 +6,40 @@ labelsTrain = loadMNISTLabels('train-labels.idx1-ubyte');
 imagesTest = loadMNISTImages('t10k-images.idx3-ubyte');
 labelsTest = loadMNISTLabels('t10k-labels.idx1-ubyte');
 
+%% functions
+rlu = @(w,x,b) max(0,w*x+b);
+sigmoid = @(w,x,b) 1./(1+exp((w*x + b)));
 %% Training!
-N = 60000; %number of datapoints
+N = 6000; %number of datapoints
 h = 2000; % number of hidden layers
 
+X = imagesTrain(1:end,1:N);         % Smaller feature matrix 
+sublables = labelsTrain(1:N,1);     % correspoinding lables
 
+B = ones(h,size(X,2));  % Initialize bias term
+C = 2*rand(1,N)-1;
 
-X = imagesTrain(1:end,1:N);% Smaller feature matrix 
-sublables = labelsTrain(1:N,1);  % correspoinding lables
+Wr = dictmake( h, size(X,1));      % random weights [-1,1]
 
-B = ones(h,size(X,2)); % Initialize bias term
-Wr = dictmake( h , size(X,1)); % random weights
-sigma = 1./(1+exp(Wr*X + B)); % sigmoid function
+sigma = rlu(Wr,X,B);     % sigmoid function (is this kernel?)
+         
 
-Y = zeros(10,size(X,2));% Initialize class matrix
-%fill class matrix
+Y = zeros(10,size(X,2));            % Initialize class matrix
+
+% Fill class matrix
 for i = 1:size(sublables,1)
     Y(sublables(i,1)+1,i) = 1;
 end
 
-W1 = Y*pinv(sigma); % Calculated weiths in output layer
-
+W1 = Y*pinv(sigma);                 % Calculated weiths in output layer
 
 %% Testing
 Xtest = imagesTest;
 Ytest = labelsTest';
 Btest = ones(h,size(Xtest,2));
+Ctest = 2*rand(1,size(Xtest,2))-1;
 
-sigmatest = 1./(1+exp(Wr*Xtest + Btest));
+sigmatest = rlu(Wr,Xtest,Btest);
 
 Yres = W1 * sigmatest;
 [maxval maxind] = max(Yres,[],1);
