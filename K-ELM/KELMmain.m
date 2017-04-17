137ded18e51df1c8465754d05d57ccfc06964846
Erik Rosvall <erik.rosvall@gmail.com>
2017-04-17 10:22:12 +0200

    mergin them changes

--- K-ELM/KELMmain.m
+++ K-ELM/KELMmain.m
@@ -2,15 +2,17 @@ clear all
 
 kernel = 'poly';
 cd /Users/erikrosvall/github/KEX/K-ELM/
+addpath('~/Dropbox/Kex/Datasets/Data från Ayman');
 %% MNIST
 
 load MNISTData.mat
 MNISTacc = [];
 MNISTtimeTrain = [];
 MNISTtimeClass = [];
-for datasize = 5000:5000:5000
-    X = imagesTrain(:,1:datasize);
-    L = labelsTrain(1:datasize,1)'+1;
+datasize = 10000:5000:10000;
+for i = datasize
+    X = imagesTrain(:,1:i);
+    L = labelsTrain(1:i,1)'+1;
 
     hiddenNodes = size(X,1)*2;
     lambda = 1;
@@ -32,4 +34,40 @@ end
 clear imagesTrain imagesTest labelsTest labelsTrain sigma testL wi wo X L t
 save(strcat('MNIST',kernel));
 
+%% RANDOM FACES AR
+
+load randomfaces4AR.mat
+
+ARacc = [];
+ARtimeTrain = [];
+ARtimeTrain = [];
+datasize = 100:1:100;
+for i = datasize
+    X = featureMat(:,1:i);
+    L = labelMat(:,1:i);
+
+    hiddenNodes = size(X,1)*2;
+    lambda = 1;
+    
+    t = cputime();
+    [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);
+    ARtimeTrain = [ARtimeTrain, cputime - t];
+    
+    %l = KELMclassifier(X,sigma,wi,wo,kernel,1);
+    %trainAccuracy = nnz(l == L)/size(l,2)
+    t = cputime();
+    testL = KELMclassifier(imagesTest,sigma,wi,wo,kernel,kernelparam);
+    ARtimeTrain  = [ARtimeTrain, cputime - t];
+    
+    
+    ARacc = [ARacc, nnz(testL == labelsTest'+1)/size(testL,2)];    
+end
+
+clear featureMat filenameMat labelMat
+save(strcat('AR',kernel))
+
 %% YALEFACES EXTENDED
+
+%% CALTECH101
+
+%% SCENE15
