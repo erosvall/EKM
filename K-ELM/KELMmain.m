88cfc3937cfb16a4ee1fcc4b00b03288c32765a8
Erik Rosvall <erik.rosvall@gmail.com>
2017-04-17 11:26:03 +0200

    Nu funkar AR datasetet också

--- K-ELM/KELMmain.m
+++ K-ELM/KELMmain.m
@@ -42,14 +42,20 @@ load randomfaces4AR.mat
 
 ARacc = [];
 ARtimeTrain = [];
-ARtimeTrain = [];
-datasize = 100:1:100;
+ARtimeClass = [];
+datasize = 0.1:0.1:0.9;
+% Rearrange all the classes
+rng(420)
+a = randperm(2600);
+featureMat = featureMat(:,a);
+labelMat = labelMat(:,a);
+
 for i = datasize
-    X = featureMat(:,1:i);
-    L = labelMat(:,1:i);
+    X = featureMat(:,1:round(2600*i));
+    L = labelMat(:,1:round(2600*i));
 
     hiddenNodes = size(X,1)*2;
-    lambda = 1;
+    lambda = 1e21;
     
     t = cputime();
     [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);
@@ -58,14 +64,15 @@ for i = datasize
     %l = KELMclassifier(X,sigma,wi,wo,kernel,1);
     %trainAccuracy = nnz(l == L)/size(l,2)
     t = cputime();
-    testL = KELMclassifier(imagesTest,sigma,wi,wo,kernel,kernelparam);
-    ARtimeTrain  = [ARtimeTrain, cputime - t];
+    testL = KELMclassifier(featureMat(:,round(2600*i) + 1:end),sigma,wi,wo,kernel,kernelparam);
+    ARtimeClass  = [ARtimeClass, cputime - t];
     
+    [~,class] = max(labelMat(:,round(2600*i) + 1:end));
     
-    ARacc = [ARacc, nnz(testL == labelsTest'+1)/size(testL,2)];    
+    ARacc = [ARacc, nnz(testL == class)/size(testL,2)];    
 end
 
-clear featureMat filenameMat labelMat
+clear featureMat filenameMat labelMat a
 save(strcat('AR',kernel))
 
 %% YALEFACES EXTENDED
