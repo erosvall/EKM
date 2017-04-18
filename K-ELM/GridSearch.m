ef02646fea097cf057b314d5543f177a0995b7d7
Viktor Karlsson <Viktor2k>
2017-04-18 21:50:51 +0200

    redo för GS

--- K-ELM/GridSearch.m
+++ K-ELM/GridSearch.m
@@ -11,14 +11,19 @@ Yt = labelsTest'+1;
 %% KELM Grid serach
 kernel = 'rbf';
 lambda = [1:2];
-kernelparam = [1:10];
+kernelparam = [1:2];
 Accuracy = [];
+params = [];
+h = size(X,1)*2;
 for l = lambda
     for kp = kernelparam
-        Accuracy = [Accuracy; l kp KELMClassificationAccuracy(X,Y,Xt,Yt,l,kernel,kp)]
+        Accuracy = [Accuracy; l kp KELMClassificationAccuracy(X,Y,Xt,Yt,l,h,kernel,kp)];
+        params = [params;l kp];
     end
 end
 [maxAccuracy,maxIndex] = max(Accuracy(:,3));
+maxAccuracy;
+params(maxIndex,:);
 
 
 
