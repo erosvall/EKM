3789aa30ba66ab5491db0a4a1183d9f5cb116cb4
Erik Rosvall <erik.rosvall@gmail.com>
2017-04-18 21:47:58 +0200

    Lagt till så att vi kan göra gridsearch på andra dataset än MNIST

--- K-ELM/GridSearch.m
+++ K-ELM/GridSearch.m
@@ -8,6 +8,30 @@ Xt = imagesTest;
 Y = labelsTrain(1:datasize)'+1;
 Yt = labelsTest'+1;
 
+
+%% För alla andra dataset
+addpath('~/Dropbox/Kex/Datasets/Data från Ayman');
+load('randomfaces4AR')
+% randomfaces4extendedyaleb
+% randomfaces4AR
+% spatialpyramidfeatures4scene15
+% spatialpyramidfeatures4caltech101
+
+N = size(featureMat,2);
+% Väljer alltid 70% av datasetet.
+% Shuffle för att se till att dataseten blir användvara
+a = randperm(N);
+featureMat = featureMat(:,a);
+labelMat = labelMat(:,a);   
+
+X = featureMat(:,1:round(N*0.7));
+Xt = featureMat(:,round(N*0.7) + 1:end);
+
+Y = labelMat(:,1:round(N*0.7));
+Yt = labelMat(:,round(N*0.7) + 1:end);
+
+
+
 %% KELM Grid serach
 kernel = 'rbf';
 lambda = [1:2];
