ddcf4fd4152884477c4a9c6e0ee2fd93443b807f
Viktor Karlsson <Viktor2k>
2017-04-19 12:52:47 +0200

    städat upp i kelmmain för enklare plottar osv

--- K-ELM/KELMmain.m
+++ K-ELM/KELMmain.m
@@ -15,133 +15,131 @@ load MNISTData.mat
 accuracy = [];
 trainTime = [];
 classificationTime = [];
-gridSearchSave=[];
+
 % --------------------------
 datasize = [5000:5000:5000];
-kernelparam = [1:2];
-lambda = [1e0,1e1];
+kernelparam = 1;
+lambda= 10;
 % --------------------------
 for d = datasize
     
     X = imagesTrain(:,1:d);
     L = labelsTrain(1:d,1)'+1;
     hiddenNodes = size(X,1)*2;
+
+   
+    % Train model
+    t = cputime();
+    [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);
+    trainTime = [trainTime, cputime - t];
     
-    for l = lambda    
-        for kp = kernelparam
-            % Train model
-            t = cputime();
-            [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,l,kernel,kp);
-            tempTrainTime = cputime - t;
-            trainTime = [trainTime, tempTrainTime];
+    % Test model
+    t = cputime();
+    testL = KELMclassifier(imagesTest,sigma,wi,wo,kernel,kernelparam); 
+    classificationTime  = [classificationTime, cputime - t];
+    
+    accuracy = [accuracy, nnz(testL == labelsTest'+1)/size(testL,2)];
 
-            
-            % Test model
-            t = cputime();
-            testL = KELMclassifier(imagesTest,sigma,wi,wo,kernel,kp);
-            tempClassificationTime = cputime - t;
-            classificationTime  = [classificationTime, tempClassificationTime];
-            
-            tempAccuracy = nnz(testL == labelsTest'+1)/size(testL,2);
-            accuracy = [accuracy, tempAccuracy];
-            
-            gridSearchSave = [gridSearchSave; d l kp tempAccuracy tempTrainTime tempClassificationTime];
-        end        
-    end
+           
+                
 end
+acc = accuracy;
+classTime = classificationTime;
 
-[maxAccuracy,maxIndex] = max(gridSearchSave(:,4));
-
-clear imagesTrain imagesTest labelsTest labelsTrain sigma testL wi wo X L t
+clear  accuracy d classificationTime tempTrainTime imagesTrain imagesTest labelsTest labelsTrain sigma testL wi wo X L t
 save(strcat('MNIST',kernel,num2str(kernelparam)));
-
-%clear all
+clear all
 toc
 %% RANDOM FACES AR
 kernel = 'rbf';
-rng(420)
+datasize = [0.3:0.05:0.8];
+kernelparam = [1];
+lambda = [1e0];
+iterations = 2;
+%--------------------------
 
+rng(420)
 tic
 disp('AR')
 load randomfaces4AR.mat
 
-accuracy = [];
+acc = [];
 trainTime = [];
-classificationTime = [];
-gridSearchSave=[];
-%--------------------------
-datasize = [0.3:0.05:0.8];
-kernelparam = [1:2];
-lambda = [1e0,1e1];
-iterations = 100;
-%--------------------------
+classTime = [];
+
+N = size(featureMat,2);
+
 for j = 1:iterations
-    a = randperm(size(featureMat, 2));
+    % suffle data
+    a = randperm(N);
     featureMat = featureMat(:,a);
     labelMat = labelMat(:,a);   
     
+    % temp save vectors
     tempAcc = [];
     tempTrainTime = [];
-    tempClassificationTime = [];
+    tempClassTime = [];
+    
     for d = datasize
-        for l = lambda
-            for kp = kernelparam
-                X = featureMat(:,1:round(N*d));
-                L = labelMat(:,1:round(N*d));
 
-                hiddenNodes = size(X,1)*2;
-                lambda = 1e21;
+        X = featureMat(:,1:round(N*d));
+        L = labelMat(:,1:round(N*d));
 
-                t = cputime();
-                [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,l,kernel,kp);
-                tempTime
-                timeTrain = [timeTrain, cputime - t];
+        hiddenNodes = size(X,1)*2;
+        
+        t = cputime();
+        [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);                
+        tempTrainTime = [tempTrainTime, cputime - t];
 
-                %l = KELMclassifier(X,sigma,wi,wo,kernel,1);
-                %trainAccuracy = nnz(l == L)/size(l,2)
-                t = cputime();
-                testL = KELMclassifier(featureMat(:,round(N*d) + 1:end),sigma,wi,wo,kernel,kp);
-                timeClass  = [timeClass, cputime - t];
 
-                [~,class] = max(labelMat(:,round(N*d) + 1:end));
+        t = cputime();
+        testL = KELMclassifier(featureMat(:,round(N*d) + 1:end),sigma,wi,wo,kernel,kernelparam);
+        tempClassTime  = [tempClassTime, cputime - t];
 
-                acc = [acc, nnz(testL == class)/size(testL,2)];    
-            end
-        end                
+        [~,class] = max(labelMat(:,round(N*d) + 1:end));
+
+        tempAcc = [tempAcc, nnz(testL == class)/size(testL,2)];    
+                
     end
-    ARresAcc = [ARresAcc; acc];
-    ARresTrainTime = [ARresTrainTime; timeTrain];
-    ARresClassTime = [ARresClassTime; timeClass];
+    acc = [acc; tempAcc];
+    trainTime = [trainTime; tempTrainTime];
+    classTime = [classTime; tempClassTime];
 end
 
-clear featureMat filenameMat labelMat a timeTrain acc timeTrain wi wo sigma X L
-save(strcat('AR',kernel,'_p=1'))
-clear ARresAcc ARresTrainTime ARresClassTime timeClass testL t N lambda j i hiddenNodes datasize class
+acc = mean(acc)
+trainTime = mean(trainTime);
+classTime = mean(classTime);
+clear tempAcc tempClassTime tempTrainTime featureMat filenameMat labelMat a timeTrain timeTrain wi wo sigma X L class d iteration j N t testL
+save(strcat('AR',kernel,num2str(kernelparam)))
+clear all
 toc
 %% YALEFACES EXTENDED
+kernel = 'rbf';
+kernelparam = 1;
+lambda = 1e8;
+datasize = 0.3:0.1:0.8;
+iterations = 2;
+
 tic
 disp('yalefaces')
 load randomfaces4extendedyaleb.mat
 
-
 N = size(featureMat, 2);
-datasize = 0.3:0.1:0.8;
-% Rearrange all the classes
+
 rng(420)
-
-
-YFresAcc = [];
-YFresTrainTime = [];
-YFresClassTime = [];
-
-
-for j = 1:100
+acc = [];
+trainTime = [];
+classTime = [];
+for j = 1:iterations
+    % Rearrange all the classes
     a = randperm(N);
     featureMat = featureMat(:,a);
     labelMat = labelMat(:,a);    
-    acc = [];
-    timeTrain = [];
-    timeClass = [];
+    
+    % create temp storage
+    tempAcc = [];
+    tempTrainTime = [];
+    tempClassTime = [];
     for i = datasize
         X = featureMat(:,1:round(N*i));
         L = labelMat(:,1:round(N*i));
@@ -151,44 +149,52 @@ for j = 1:100
 
         t = cputime();
         [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);
-        timeTrain = [timeTrain, cputime - t];
+        tempTrainTime = [tempTrainTime, cputime - t];
 
         %l = KELMclassifier(X,sigma,wi,wo,kernel,1);
         %trainAccuracy = nnz(l == L)/size(l,2)
         t = cputime();
         testL = KELMclassifier(featureMat(:,round(N*i) + 1:end),sigma,wi,wo,kernel,kernelparam);
-        timeClass  = [timeClass, cputime - t];
+        tempClassTime  = [tempClassTime, cputime - t];
 
         [~,class] = max(labelMat(:,round(N*i) + 1:end));
 
-        acc = [acc, nnz(testL == class)/size(testL,2)];    
+        tempAcc = [tempAcc, nnz(testL == class)/size(testL,2)];    
     end
-    YFresAcc = [YFresAcc;acc];
-    YFresTrainTime = [YFresTrainTime;timeTrain];
-    YFresClassTime = [YFresClassTime;timeClass];
+    acc = [acc;tempAcc];
+    trainTime = [trainTime;tempTrainTime];
+    classTime = [classTime;tempClassTime];
 end
-clear featureMat filenameMat labelMat a timeTrain acc timeTrain wi wo sigma X L
-save(strcat('YF',kernel,'_p=1'))
-clear YFresAcc YFresTrainTime YFresClassTime class datasize hiddenNodes i j lambda N t testL timeClass
+
+acc = mean(acc);
+trainTime = mean(trainTime);
+classTime = mean(classTime);
+
+clear featureMat filenameMat labelMat class i j N t  tempAcc tempClassTime tempTrainTime testL a  wi wo sigma X L
+save(strcat('YF',kernel,num2str(kernelparam)))
+clear all
 toc
 %% CALTECH101
+kernel = 'rbf';
+kernelparam = 1;
+lambda = 1e8;
+datasize = 0.3:0.1:0.8;
+iterations = 2;
+% ----------------
+
 tic
 disp('caltech')
 load spatialpyramidfeatures4caltech101.mat
-
-
 N = size(featureMat, 2);
-datasize = 0.3:0.05:0.8;
-% Rearrange all the classes
 rng(420)
-
 
 CalTech101resAcc = [];
 CalTech101resTrainTime = [];
 CalTech101resClassTime = [];
 
 
-for j = 1:10
+for j = 1:iterations
+    % Rearrange all the classes
     a = randperm(N);
     featureMat = featureMat(:,a);
     labelMat = labelMat(:,a);    
@@ -220,30 +226,33 @@ for j = 1:10
     CalTech101resTrainTime = [CalTech101resTrainTime;timeTrain];
     CalTech101resClassTime = [CalTech101resClassTime;timeClass];
 end
+acc = mean(CalTech101resAcc);
+classTime = mean(CalTech101resClassTime);
+trainTime = mean(CalTech101resTrainTime);
 
-clear featureMat filenameMat labelMat a timeTrain acc timeTrain wi wo sigma X L
-save(strcat('CalTech101',kernel,'_p=1'))
-clear CalTech101resAcc CalTech101resClassTime CalTech101resTrainTime class datasize hiddenNodes i j lambda N t testL timeClass
-
+clear featureMat filenameMat labelMat CalTech101resAcc timeTrain timeClass CalTech101resClassTime class i j N t testL CalTech101resTrainTime a wi wo sigma X L
+save(strcat('CalTech101',kernel,num2str(kernelparam)))
+clear all
 toc
 %% SCENE15
+kernel = 'rbf';
+kernelparam = 1;
+lambda = 1e-7;
+datasize = 0.5:0.1:0.6;
+iterations = 2;
+% ----------------
+
 tic
 load spatialpyramidfeatures4scene15.mat
 disp('scene15')
-
 N = size(featureMat, 2);
-
-datasize = 0.3:0.05:0.8;
-% Rearrange all the classes
 rng(420)
-
-
 scene15resAcc = [];
 scene15resTrainTime = [];
 scene15resClassTime = [];
 
 
-for j = 1:10
+for j = 1:iterations
     a = randperm(N);
     featureMat = featureMat(:,a);
     labelMat = labelMat(:,a);    
@@ -255,8 +264,7 @@ for j = 1:10
         L = labelMat(:,1:round(N*i));
 
         hiddenNodes = size(X,1)*2;
-        lambda = 1e-7;
-
+        
         t = cputime();
         [wi, wo, sigma] = KELMtrainer(X,L,hiddenNodes,lambda,kernel,kernelparam);
         timeTrain = [timeTrain, cputime - t];
@@ -275,8 +283,11 @@ for j = 1:10
     scene15resTrainTime = [scene15resTrainTime;timeTrain];
     scene15resClassTime = [scene15resClassTime;timeClass];
 end
+acc = mean(scene15resAcc);
+trainTime = mean(scene15resTrainTime);
+classTime = mean(scene15resClassTime);
 
-clear featureMat filenameMat labelMat a timeTrain acc timeTrain wi wo sigma X L
-save(strcat('scene15',kernel))
-clear class datasize hiddenNodes i j lambda N scene15resAcc scene15resClassTime scene15resTrainTime t testL timeClass
+clear featureMat filenameMat scene15resClassTime scene15resTrainTime timeClass scene15resAcc class i j N t testL labelMat a timeTrain timeTrain wi wo sigma X L
+save(strcat('scene15',kernel,num2str(kernelparam)))
+clear all
 toc
